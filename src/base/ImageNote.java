237ab384350fd32b9f2d2726368c0a0fe7d9a535
720s-15 18sb <720s-15 18sb@NBManG>
2022-02-23 13:56:21 +0800

    Lab2 commit

---
+++ src/base/ImageNote.java
@@ -0,0 +1,13 @@
+package base;
+
+import java.io.File;
+
+public class ImageNote extends Note{
+	
+	private File image;
+	public ImageNote(String title) {
+		// TODO Auto-generated constructor stub
+		super(title);
+	}
+
+}
